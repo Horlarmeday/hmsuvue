82ab6869c3288583cbc85ff6da437ee0ee300be8
Mahmud <ajaomahmud@gmail.com>
2020-01-11 20:29:40 +0100

    chore: set the public IP

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 // vue.config.js
-// module.exports = {
-//   devServer: {
-//     proxy: process.env.VUE_APP_BACKEND_URL
-//   }
-// }
+module.exports = {
+  // devServer: {
+  //   proxy: process.env.VUE_APP_BACKEND_URL
+
+  // }
+  devServer: {
+    host: '0.0.0.0',
+    port: '8080',
+    public: '192.168.2.172:8080'
+  }
+}
